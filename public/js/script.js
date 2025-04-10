1681e5fb69679d9d6567e4ca60c3e2c36a15c046
bilalahmet.akpinar <akpinarbilalahmet@gmail.com>
2025-04-10 09:03:12 +0300

    project done

--- public/js/script.js
+++ public/js/script.js
@@ -26,9 +26,41 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         body: data
       });
-
+      
+      /* // document.body.innerHTML = resultHTML ile tüm HTML’yi değiştiriyoruz bu yüzden controller'da json formatına geçildi sadece toast mesajı için yapılan bir işlem
       const resultHTML = await response.text(); // sunucudan gelen yanıtı alır
-      document.body.innerHTML = resultHTML; // sayfanın içeriğini günceller
+      document.body.innerHTML = resultHTML; // sayfanın içeriğini günceller */
+
+      const result = await response.json(); // sunucudan gelen yanıtı alır
+      showToast(result.message, result.status); // toast mesajını gösterir
     });
+  }
+
+  function showToast(message, status){
+    
+    // önce varsa eski toast mesajını temizler
+    const existingToast = document.getElementById('liveToast');
+    if(existingToast) existingToast.remove();
+
+    // toast HTML oluşturma
+    const toast = document.createElement('div');
+    toast.id = 'liveToast';
+    toast.className = `toast align-items-center text-bg-${status === 'success' ? 'success' : 'danger'} show position-fixed bottom-0 end-0 m-4`;
+    toast.role = 'alert';
+    toast.style.zIndex = 1050; // Bootstrap toast z-index değeri
+    toast.innerHTML = `
+      <div class="d-flex">
+        <div class="toast-body">${message}</div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"></button>
+      </div>
+    `;
+
+    document.body.appendChild(toast);
+
+    const bsToast = new bootstrap.Toast(toast, {
+      delay: 3000,
+      autohide: true
+    });
+    bsToast.show();
   }
 });
